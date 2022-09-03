ef6ec869be07bbc875b624d98325c1323e44fd7d
hongchanglin <2278023068@qq.com>
2022-09-03 21:27:20 +0800

    init

---
+++ fegin/src/main/java/com/example/fegin/FeginApplication.java
@@ -0,0 +1,13 @@
+package com.example.fegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeginApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FeginApplication.class, args);
+  }
+
+}
